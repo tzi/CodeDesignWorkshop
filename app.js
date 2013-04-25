23216fe2241d2a02e6eb8ac4a8e2147c1e069695
Thomas ZILLIOX <thomas@zilliox.me>
2013-04-25 02:13:58 +0200

    Separate application platform code and project code

--- app.js
+++ app.js
@@ -1,54 +1,71 @@
-function App() {
-    var state = true;
-    QUnit.done( function(details){
-       if ( details.failed > 0 ) {
-           state = false;
-       }
+App = (function App() {
+
+    // Initialization
+    var editor = ace.edit("editorFrame");
+    editor.setTheme("ace/theme/twilight");
+    editor.getSession().setMode("ace/mode/javascript");
+    QUnit.testDone(function(details){
+        addTestReport(details.name, details.failed==0);
     });
 
-    var testReport = function() {
-        if ( state ) {
-            alert( 'All tests are great :)');
+    // Private
+    cleanTestReport = function() {
+        document.getElementById('testFrame').innerHTML = '';
+    }
+    addTestReport = function(name, result) {
+        document.getElementById('testFrame')
+            .innerHTML += '<span class="test">'
+            + '<span class="test-name">' + name +'</span>: '
+            + '<span class="test-result test-result--' + (result?'ok':'error') + '">' + (result?'OK':'ERROR') + '</span>'
+            + '</span>';
+    }
+    toggleTestReport = function(action) {
+        var method;
+        if (typeof action == 'object' || typeof action == 'undefined') {
+            action = false;
+            method='toggle';
+        } else if (action) {
+            method='add';
         } else {
-            alert( 'There is an error in the tests');
+            method='remove';
+        }
+        var result = document.getElementById('mainFrame').classList[method]('showTest');
+        var button = document.getElementById('showTestButton');
+        if (result || action) {
+            button.innerHTML = 'Hide tests';
+        } else {
+            button.innerHTML = 'Show tests';
         }
     }
 
-    this.toggle = function() {
-        if ( document.getElementById('mainFrame').classList.toggle('showTest') ) {
-            this.innerHTML = 'Hide tests';
-        } else {
-            this.innerHTML = 'Show tests';
+    return {
+        toggleTestReport: function(action) {
+            toggleTestReport(action);
+        },
+        runTests: function() {
+            var js = editor.getValue();
+            try {
+                eval(js);
+            } catch(e) {
+                alert('Error in javascript compilation');
+                return false;
+            }
+            cleanTestReport();
+            toggleTestReport(true);
+            QUnit.reset();
+            QUnit.init();
+            QUnit.start();
+            TestSuite(js);
+        },
+        init: function(val) {
+            editor.setValue(val);
         }
-    };
-
-    this.run = function() {
-        try {
-            eval( editor.getValue() );
-            state = true;
-            QUnit.test( 'Hello world test', function(){
-                QUnit.equal( new GoBan().getName(), 'Hello World' );
-            });
-            QUnit.test( 'Hello world test', function(){
-                QUnit.equal( new GoBan().getName(), 'Hello World' );
-            });
-            testReport();
-        } catch(e) {
-            alert('Error in javascript compilation');
-        }
-    };
-}
-
-
-// Editor
-var editor = ace.edit("editorFrame");
-editor.setTheme("ace/theme/twilight");
-editor.getSession().setMode("ace/mode/javascript");
+    }
+})();
 
 // Menu
-var app = new App();
 var showTestButton = document.getElementById('showTestButton');
-showTestButton.addEventListener('click', app.toggle);
+showTestButton.addEventListener('click', App.toggleTestReport);
 var runTestButton = document.getElementById('runTestButton');
-runTestButton.addEventListener('click', app.run);
+runTestButton.addEventListener('click', App.runTests);
 
---
+++ project/hello-world.js
@@ -0,0 +1,12 @@
+App.init((function GoBan() {
+    this.getName = function() {
+        return 'Hello World';
+    };
+}).toString());
+
+function TestSuite(js) {
+    eval(js);
+    QUnit.test('Hello world test suite', function(){
+        QUnit.equal(new GoBan().getName(), 'Hello World');
+    });
+}
