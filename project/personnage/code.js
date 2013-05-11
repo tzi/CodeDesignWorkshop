2bb3af9687aec93648e8e03c007006c2e25c8535
Tûtie <tut.tuuut@gmail.com>
2013-05-11 10:58:55 +0200

    Adds the ability to create a character with custom params.

--- project/personnage/code.js
+++ project/personnage/code.js
@@ -1,6 +1,26 @@
 function generateCharacter(params) {
-    var priv = {};
+    var priv = {
+        str: 1,
+        def: 1,
+        pv: 10
+    };
     var pub = {};
+
+    if (params) {
+        for (carac in params) {
+            priv[carac] = params[carac];
+        }
+    }
+
+    pub.getStr = function() {
+        return priv.str;
+    }
+    pub.getDef = function() {
+        return priv.def;
+    }
+    pub.getPv = function() {
+        return priv.pv;
+    }
 
     return pub;
 }
--- project/personnage/testSuite.js
+++ project/personnage/testSuite.js
@@ -1,1 +1,11 @@
-var perso = generateCharacter();
+QUnit.test('You can initialize a character with given parameters', function(){
+	var params = {
+		str: 5,
+		def: 6,
+		pv: 37
+	};
+	var perso = generateCharacter(params);
+    QUnit.equal(perso.getStr(), 5);
+    QUnit.equal(perso.getDef(), 6);
+    QUnit.equal(perso.getPv(), 37);
+});
