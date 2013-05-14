ddae933178bc95ac0e4a96a295db37121b09303c
Thomas ZILLIOX <thomas@zilliox.me>
2013-05-14 16:57:25 +0200

    Improve test code readability

--- project/url/testSuite1.js
+++ project/url/testSuite1.js
@@ -1,4 +1,6 @@
-QUnit.test('For empty arguments, return document url', function(){
+/* Exercise 1
+ ==================================================================*/
+QUnit.test('Instantiate Url without argument', function(){
     var url = new Url();
     QUnit.equal(url.full, window.location.href);
     QUnit.equal(url.protocol+':', window.location.protocol);
@@ -6,35 +8,41 @@ QUnit.test('For empty arguments, return document url', function(){
     QUnit.equal(url.domain, window.location.host);
     QUnit.equal(url.path, window.location.pathname);
 });
-QUnit.test('For window, return associate url', function(){
+
+QUnit.test('Instantiate Url with the current window', function(){
     var url = new Url(window);
     QUnit.equal(url.full, window.location.href);
     QUnit.equal(url.protocol+':', window.location.protocol);
 });
-QUnit.test('For document, return associate url', function(){
+
+QUnit.test('Instantiate Url with the current document', function(){
     var url = new Url(document);
     QUnit.equal(url.full, window.location.href);
     QUnit.equal(url.protocol+':', window.location.protocol);
 });
-QUnit.test('For image, return associate url', function(){
+
+QUnit.test('Instantiate Url with the an IMG element', function(){
     var el = document.createElement('img')
     el.setAttribute('src', 'http://example.net')
     var url = new Url(el);
     QUnit.equal(url.full, 'http://example.net/');
 });
-QUnit.test('For link, return associate url', function(){
+
+QUnit.test('Instantiate Url with the an A element', function(){
     var el = document.createElement('a')
     el.setAttribute('href', 'http://example.net')
     var url = new Url(el);
     QUnit.equal(url.full, 'http://example.net/');
 });
-QUnit.test('For iframe, return associate url', function(){
+
+QUnit.test('Instantiate Url with the an IFRAME element', function(){
     var el = document.createElement('iframe')
     el.setAttribute('src', 'http://example.net')
     var url = new Url(el);
     QUnit.equal(url.full, 'http://example.net/');
 });
-QUnit.test('For form, return associate url', function(){
+
+QUnit.test('Instantiate Url with the a FORM element', function(){
     var el = document.createElement('form')
     el.setAttribute('action', 'http://example.net')
     var url = new Url(el);
--- project/url/testSuite2.js
+++ project/url/testSuite2.js
@@ -1,4 +1,6 @@
-QUnit.test('For empty arguments, return document url', function(){
+/* Exercise 1
+ ==================================================================*/
+QUnit.test('Instantiate Url without argument', function(){
     var url = new Url();
     QUnit.equal(url.full, window.location.href);
     QUnit.equal(url.protocol+':', window.location.protocol);
@@ -6,35 +8,41 @@ QUnit.test('For empty arguments, return document url', function(){
     QUnit.equal(url.domain, window.location.host);
     QUnit.equal(url.path, window.location.pathname);
 });
-QUnit.test('For window, return associate url', function(){
+
+QUnit.test('Instantiate Url with the current window', function(){
     var url = new Url(window);
     QUnit.equal(url.full, window.location.href);
     QUnit.equal(url.protocol+':', window.location.protocol);
 });
-QUnit.test('For document, return associate url', function(){
+
+QUnit.test('Instantiate Url with the current document', function(){
     var url = new Url(document);
     QUnit.equal(url.full, window.location.href);
     QUnit.equal(url.protocol+':', window.location.protocol);
 });
-QUnit.test('For image, return associate url', function(){
+
+QUnit.test('Instantiate Url with the an IMG element', function(){
     var el = document.createElement('img')
     el.setAttribute('src', 'http://example.net')
     var url = new Url(el);
     QUnit.equal(url.full, 'http://example.net/');
 });
-QUnit.test('For link, return associate url', function(){
+
+QUnit.test('Instantiate Url with the an A element', function(){
     var el = document.createElement('a')
     el.setAttribute('href', 'http://example.net')
     var url = new Url(el);
     QUnit.equal(url.full, 'http://example.net/');
 });
-QUnit.test('For iframe, return associate url', function(){
+
+QUnit.test('Instantiate Url with the an IFRAME element', function(){
     var el = document.createElement('iframe')
     el.setAttribute('src', 'http://example.net')
     var url = new Url(el);
     QUnit.equal(url.full, 'http://example.net/');
 });
-QUnit.test('For form, return associate url', function(){
+
+QUnit.test('Instantiate Url with the a FORM element', function(){
     var el = document.createElement('form')
     el.setAttribute('action', 'http://example.net')
     var url = new Url(el);
@@ -42,7 +50,9 @@ QUnit.test('For form, return associate url', function(){
 });
 
 
-// Exercice 2
+
+/* Exercise 2
+ ==================================================================*/
 QUnit.test('Extract protocol from absolute url', function(){
     var url = new Url('http://example.net');
     QUnit.equal(url.protocol, 'http');
@@ -51,16 +61,19 @@ QUnit.test('Extract protocol from absolute url', function(){
     var url3 = new Url('ftp://example.net');
     QUnit.equal(url3.protocol, 'ftp');
 });
+
 QUnit.test('Extract domain from absolute url', function(){
     var url = new Url('http://example.net');
     QUnit.equal(url.protocol, 'http');
 });
+
 QUnit.test('Extract port from absolute url', function(){
     var url = new Url('http://example.net');
     QUnit.equal(url.port, 80);
     var url2 = new Url('http://example.net:418');
     QUnit.equal(url2.port, 418);
 });
+
 QUnit.test('Extract path from absolute url', function(){
     var url = new Url('http://example.net');
     QUnit.equal(url.path, '/');
@@ -71,6 +84,7 @@ QUnit.test('Extract path from absolute url', function(){
     var url4 = new Url('http://example.net/it/is?so#funny');
     QUnit.equal(url4.path, '/it/is');
 });
+
 QUnit.test('Extract query from absolute url', function(){
     var url = new Url('http://example.net');
     QUnit.equal(url.query, '');
@@ -79,6 +93,7 @@ QUnit.test('Extract query from absolute url', function(){
     var url3 = new Url('http://example.net/it/is?so&funny');
     QUnit.equal(url3.query, 'so&funny');
 });
+
 QUnit.test('Extract anchor from absolute url', function(){
     var url = new Url('http://example.net/it/is?so#funny');
     QUnit.equal(url.anchor, 'funny');
